b6a78649dbb4f930c0b35405a23b93de278a270f
Junyang Jiang <junyangJiang@outlook.com>
2020-04-25 12:58:13 -0400

    Add git show-ref and fix minor bugs

--- src/lib/commands.hpp
+++ src/lib/commands.hpp
@@ -3,6 +3,7 @@
 
 #include <filesystem>
 #include <set>
+#include <unordered_map>
 // To get the data structures
 #include <git_objects.hpp>
 /* #include <helper.hpp> */
@@ -17,9 +18,10 @@ namespace fs = std::filesystem;
 
 /* ********* Functions	********* */
 // Functions dealing with commands: validate, convert file path to absolute etc.
-void cmd_init(std::vector<std::string>& args);
-void cmd_cat_file(std::vector<std::string>& args);
-void cmd_checkout(std::vector<std::string>& args);
+void cmd_init(const std::vector<std::string>& args);
+void cmd_cat_file(const std::vector<std::string>& args);
+void cmd_checkout(const std::vector<std::string>& args);
+void cmd_show_ref(const std::vector<std::string> &args);
 // Actual functions executing commands
 void git_cat_file(fs::path obj, const std::string& fmt);
 void git_init(fs::path project_base_path);
@@ -30,4 +32,7 @@ int test_function(void);
 std::string git_add_file(const fs::path& file_path);
 std::string read_project_folder_and_write_tree(const fs::path& adding_directory, bool root = false);
 std::string git_add_folder(const fs::path folder_path);
+
+std::string ref_resolve(fs::path path);
+std::unordered_map<std::string, std::string> ref_list(fs::path base_path);
 #endif
